e6bc1ca4214f3c5245f56de21631085c34cc6e05
OeungChiva <oeungchiva98@gmail.com>
2022-11-03 14:23:51 +0700

    product_details

--- resources/views/home/product.blade.php
+++ resources/views/home/product.blade.php
@@ -13,9 +13,9 @@
                   <div class="box">
                      <div class="option_container">
                         <div class="options">
-                           <!-- <a href="" class="option1">
-                           Men's Shirt
-                           </a> -->
+                           <a href="{{url('product_details',$products->id)}}" class="option1">
+                           Product Details
+                           </a>
                            <a href="" class="option2">
                            Buy Now
                            </a>
