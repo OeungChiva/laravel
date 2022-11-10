926433a42fc03f7ea498a1a65dc2b28adbfa7797
OeungChiva <oeungchiva98@gmail.com>
2022-11-10 23:30:13 +0700

    order

--- resources/views/home/show_cart.blade.php
+++ resources/views/home/show_cart.blade.php
@@ -56,6 +56,13 @@
          @include('home.header')
          <!-- end header section -->
 
+         @if(session()->has('message'))
+            <div class="alert alert-success">  
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+              {{session()->get('message')}}
+            </div>
+          @endif
+
          <div class="center">
         <table>
             <tr>
@@ -83,6 +90,13 @@
         </table>
         <div>
          <h1 class="total_deg">Total Price : {{$totalprice}}$</h1>
+
+        </div>
+
+        <div>
+         <h1 style="font-size: 25px; padding-bottom: 15px;">Proceed to Order</h1>
+         <a href="{{url('cash_order')}}"class="btn btn-danger">Cash On Delivery</a>
+         <a href=""class="btn btn-danger">Pay Using Card</a>
 
         </div>
 
